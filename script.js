5854a059177682b2e14251c04429806aa19e44cf
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2023-12-21 23:15:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let scrollcontainer = document.querySelector(".gallery");
+let backbtn = document.getElementById("backbtn");
+let nextbtn = document.getElementById("nextbtn");
+
+scrollcontainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollcontainer.scrollLeft += evt.deltaY;
+    scrollcontainer.style.scrollBehavior = "auto";
+});
+
+nextbtn.addEventListener("click", () => {
+    scrollcontainer.style.scrollBehavior = "smooth";
+    scrollcontainer.scrollLeft += 900;
+});
+
+backbtn.addEventListener("click", () => {
+    scrollcontainer.style.scrollBehavior = "smooth";
+    scrollcontainer.scrollLeft -= 900;
+});
